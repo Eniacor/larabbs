ea4c0ed738d9b2454ab7979d21912657e859f92f
vagrant <vagrant@homestead>
2017-11-25 10:08:00 +0000

    add topic1

---
+++ resources/views/topics/_sidebar.blade.php
@@ -0,0 +1,12 @@
+@if(count($topics))
+    <ul class="media-list">
+        <li class="media">
+            <div class="media-left">
+                <a href="route('users.show',[$topic->user_id])">
+                    <img class="media-object img-thumbnail" style="width:52px;height:52px;" src="{{$topic->user->avatar}}" title="{{$topic->user->name}}">
+                </a>
+            </div>
+        </li>
+    </ul>
+    
+@endif
