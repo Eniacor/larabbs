f6f5e04bb608f2499f67ddd979e7ccfda140d9ae
vagrant <vagrant@homestead>
2017-12-02 09:15:31 +0000

    Topic order

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -13,4 +13,30 @@ class Topic extends Model
     {
         return $this->belongsTo(User::class);
     }
+    /* topics order */
+    public function scopeWithOrder($query,$order)
+    {
+        //不同的排序，使用不同的数据读取逻辑
+        switch($order){
+            case 'recent':
+                $query=$this->recent();
+                break;
+            default:
+                $query=$this->recentReplied();
+                break;
+        }
+        return $query->with('user','category');
+    }
+    /**
+     * 当话题有新回复时，我们编写逻辑更新的话题模型的reply_count属性
+     * 此时会自动触发框架对数据模型updated_at时间戳的更新
+     */
+    public function scopeRecentReplied($query)
+    {
+        return $query->orderBy('updated_at','desc');
+    }
+    public function scopeRecent($query)
+    {
+        return $query->orderBy('updated_at','desc');
+    }
 }
