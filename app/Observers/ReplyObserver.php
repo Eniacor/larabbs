3a8b931c46cf8f47c90eade313c383c90b37e8ea
vagrant <vagrant@homestead>
2017-12-09 09:56:47 +0000

    notification self

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -1,12 +1,10 @@
 <?php
-
 namespace App\Observers;
-
 use App\Models\Reply;
+use App\Notifications\TopicReplied;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
-
 class ReplyObserver
 {
     public function creating(Reply $reply)
@@ -14,7 +12,12 @@ class ReplyObserver
         $reply->content=clean($reply->content,'user_topic_body');
     }
     public function created(Reply $reply)
-    {
-        $reply->topic->increment('reply_count',1);
+    {   
+        $topic=$reply->topic;
+        $topic->increment('reply_count',1);
+        //如果评论的作者不是话题的作者,才需要通知
+        if(!$reply->user->isAuthorOf($topic)){
+            $topic->user->notify(new TopicReplied($reply));
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,5 @@ Route::resource('categories','CategoriesController',['only'=>['show']]);
 Route::post('upload_image','TopicsController@uploadImage')->name('topics.upload_image');
 /* 帖子回复*/
 Route::resource('replies', 'RepliesController', ['only' => ['store','destroy']]);
+/* notify */
+Route::resource('notifications','NotificationsController',['only'=>['index']]);
