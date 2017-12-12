2d92c1d4072a40686aceba8ff9f73b68bdf6db50
vagrant <vagrant@homestead>
2017-12-12 02:47:28 +0000

    user can delete reply

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -22,11 +22,10 @@ class RepliesController extends Controller
 		return redirect()->to($reply->topic->link())->with('success', '创建成功!');
 	}
 
-	public function destroy(Reply $reply)
-	{
-		$this->authorize('destroy', $reply);
-		$reply->delete();
-
-		return redirect()->route('replies.index')->with('message', 'Deleted successfully.');
-	}
+    public function destroy(Reply $reply)
+    {
+        $this->authorize('destroy', $reply);
+        $reply->delete();
+        return redirect()->to($reply->topic->link())->with('success', '成功删除回复！');
+    }
 }
